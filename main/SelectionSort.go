6d003fde3a16e8d028cdf1b1faf95be87c5e7848
Nawazish Khan <md.nawazish.khan@gmail.com>
2016-08-16 01:19:54 +0530

    Initial commit

---
+++ main/SelectionSort.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//in every pass make the currSwapCandidate and magenta point to the first element in the sub-list
+
+func main (){
+
+	fmt.Println("selection sort starting")
+	arr:= [5]int{2,1,4,3,5}
+	//magentaIndex:=0
+	//currSwapCandidateIndex:=0
+
+	fmt.Println("unsorted array: ", arr)
+
+	//outer loop to iterate on subset of array
+	for outerLoop:=0; outerLoop < len(arr); outerLoop++{
+
+		min:=outerLoop // the first element in every outer iteration is assumed minimum
+
+		//inner loop for per-iteration comparisons
+		for innerLoop:=outerLoop+1; innerLoop< len(arr); innerLoop++ {
+			if arr[innerLoop] < arr[min]{
+				min = innerLoop
+			}
+		}
+
+		swapElements(outerLoop, min, &arr)
+	}
+	fmt.Println("sorted array: ", arr)
+}
+
+func swapElements(magentaIndex, currSwapCandidateIndex int, arr *[5]int){
+
+	temp := arr[currSwapCandidateIndex]
+	arr[currSwapCandidateIndex] = arr[magentaIndex]
+	arr[magentaIndex] = temp
+}
