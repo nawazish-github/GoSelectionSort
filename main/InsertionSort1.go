9da96142dc7846befd63ab9fef9057da5cd523d5
Nawazish Khan <md.nawazish.khan@gmail.com>
2016-08-30 22:30:24 +0530

    GoInsertionSort

---
+++ main/InsertionSort1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	//arr:= [5]int{2,1,4,3,5}
+	arr:= [9]int{2,1,4,3,5,9,7,6,8}
+
+	for out:=1; out<len(arr); out++{
+		temp:=arr[out]
+		in := out
+
+		for;in>0 && arr[in-1]>=temp;in--{
+			arr[in] = arr[in-1]
+		}
+		arr[in] = temp
+	}
+
+	for sortedvals:= range arr{
+		fmt.Println(sortedvals)
+	}
+}
